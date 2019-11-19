c9199e79cdfdff2246ca5bb8f45d345e93d7f6d1
Prasanth M <mprasanthk27@gmail.com>
2019-11-19 14:51:29 +0530

    Created Init function

--- app.js
+++ app.js
@@ -3,20 +3,53 @@ var budgetController = (function(){
 })();
 
 var uiController = (function(){
-//some code
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+return {
+  getInput: function(){
+    return {
+      type: document.querySelector(DOMstrings.inputType).value,
+      description: document.querySelector(DOMstrings.inputDescription).value,
+      value: document.querySelector(DOMstrings.inputValue).value
+    };
+  },
+  getDOMstrings: function() {
+    return DOMstrings;
+  }
+}
 })();
 
-var controller = (function(budgerCtrl, uiCtrl) {
+var controller = (function(budgetCtrl, uiCtrl) {
 
-  var ctrlAddItem = function() {
-    // Code here
-  }
+var setupEventListeners = function () {
+  var DOM = uiController.getDOMstrings();
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
   document.addEventListener('keypress', function(event){
     if(event.keyCode === 13 || event.which === 13){
       ctrlAddItem();
     }
-  })
+  });
+};
 
-}(budgetController, uiController)();
+  var ctrlAddItem = function() {
+
+    // Get the field Input data
+    var input = uiCtrl.getInput();
+  }
+
+return {
+  init: function() {
+    setupEventListeners();
+  }
+};
+
+})(budgetController, uiController);
+
+controller.init();
