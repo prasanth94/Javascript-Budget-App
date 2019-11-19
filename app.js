1e3536998a437ecc675c2565e97a4ed76aff7a2d
Prasanth M <mprasanthk27@gmail.com>
2019-11-19 16:19:35 +0530

    Add Item to UI

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var budgetController = (function(){
 
       // Create new ID
       if(data.allItems[type].length > 0){
-        data.allItems[type][data.allItems[type].length - 1].id + 1;
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
       }else{
         ID = 0;
       }
@@ -60,7 +60,9 @@ var uiController = (function(){
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
   };
 
 return {
@@ -71,6 +73,30 @@ return {
       value: document.querySelector(DOMstrings.inputValue).value
     };
   },
+
+  addListItem: function(obj, type){
+    var html, newHtml, element;
+
+    // Create HTML strings
+    if(type === 'inc'){
+      element = DOMstrings.incomeContainer;
+      html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>' ;
+    }
+    else if(type === 'exp'){
+      element = DOMstrings.expenseContainer;
+      html =  '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>' ;
+    }
+
+    // Replace the placeholder with actual values
+    newHtml = html.replace('%id%', obj.id);
+    newHtml = newHtml.replace('%description%', obj.description);
+    newHtml = newHtml.replace('%value%', obj.value);
+
+    // Add html element to DOM
+    document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+
+  },
+
   getDOMstrings: function() {
     return DOMstrings;
   }
@@ -97,6 +123,9 @@ var setupEventListeners = function () {
 
     // Add the Item to budget
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    //Add Item to UI
+    uiCtrl.addListItem(newItem, input.type);
   };
 
 return {
